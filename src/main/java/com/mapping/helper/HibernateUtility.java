576ab8ca4c1b9b4593025665751e74c76d31d7fd
Vishal Vahatule <vwahatule7@gmail.com>
2025-07-24 00:21:24 +0530

    Add pom.xml with Maven dependencies. Add Hibernate configuration file. Add HibernateUtility class for SessionFactory setup.

---
+++ src/main/java/com/mapping/helper/HibernateUtility.java
@@ -0,0 +1,21 @@
+package com.mapping.helper;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtility {
+
+	private HibernateUtility() {
+
+	}
+
+	private static SessionFactory factory;
+
+	public static SessionFactory getSessionFactory() {
+		if (factory == null) {
+			factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		}
+		return factory;
+	}
+
+}
