b3963f7761fdde47d2b0ddf06c527fcb66058d4b
Vishal Vahatule <vwahatule7@gmail.com>
2025-07-24 00:38:53 +0530

    Add class to fetch and display all Students. Add class to save new Student with Documents.

---
+++ src/main/java/com/mapping/AppGetAllData.java
@@ -0,0 +1,28 @@
+package com.mapping;
+
+import java.util.List;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.mapping.Entity.Student;
+import com.mapping.helper.HibernateUtility;
+
+public class AppGetAllData {
+
+	public static void main(String[] args) {
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		List<Student> studentList = session.createQuery("select s FROM Student s", Student.class).getResultList();
+
+		studentList.stream().forEach(k -> System.out.println(k.toString()));
+
+		transaction.commit();
+		session.close();
+
+	}
+
+}
---
+++ src/main/java/com/mapping/AppPostStudent.java
@@ -0,0 +1,42 @@
+package com.mapping;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.mapping.Entity.Documents;
+import com.mapping.Entity.Student;
+import com.mapping.helper.HibernateUtility;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class AppPostStudent 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Saving Data" );
+        SessionFactory factory = HibernateUtility.getSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+        // Documents
+        Documents documents = new Documents();
+        documents.setAadharNo(369852);
+        documents.setPanNo("ABC4576ED");
+        
+        //Student
+        Student s = new Student();
+        s.setfName("SUYASH");
+        s.setlName("DHAJEKAR");
+        s.setDocuments(documents);
+
+        session.persist(s);
+        
+        transaction.commit();
+        session.close();
+        System.out.println("DATA SAVED");
+    }
+}
