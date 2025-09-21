643d0b2ba9a03f4f842420f98d7d6b6711035b42
VISHAL SUBHASH VAHATULE <vishal.vahatule02@gmail.com>
2025-09-21 22:52:22 +0530

    Update Documents Entity

--- src/main/java/com/mapping/Entity/Documents.java
+++ src/main/java/com/mapping/Entity/Documents.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+//
 @Entity
 public class Documents {
 	@Id
